77f5cdf806c90d4ec8a72b0287b908eadb7a65d5
”Drop <drapdrop1@gmail.com>
2024-09-18 17:39:38 +0300

    icons

---
+++ frontend/src/shared/ui/icons/AddIcon.tsx
@@ -0,0 +1,24 @@
+export const AddIcon = () => (
+    <svg
+        style={{ width: '28px', height: '28px' }}
+        width="57"
+        height="57"
+        viewBox="0 0 57 57"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <rect
+            width="57"
+            height="57"
+            rx="10"
+            fill="#BBD5CD"
+        />
+        <path
+            d="M17.333 29h21.334M28 18.333v21.334"
+            stroke="#fff"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+    </svg>
+);
